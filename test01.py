6304a85b2f02a3108b3ef6a8ba0cbda6d4d89f02
tajiquanwei <tajiquanwei@gmail.com>
2018-02-04 18:30:11 +0800

    test04

--- test01.py
+++ test01.py
@@ -1,7 +1,53 @@
-for i in range( 1 , 10 ):
-    print ( i )
 
+# for i in range( 1 , 5):
+#     print ( i )
 
-a = 999.022
+import math
+import decimal
 
+a =  3**2
 print(int(a))
+
+a = pow(3, 3, 4)
+print(a)
+
+a = 7654321.3456789
+print(round(a, 0))      #7654321
+print(round(a, -1))     #7654320
+print(round(a, 1))      #7654321.3
+print(round(a, 3))      #7654321.346
+
+i = 123
+print(bin(i))
+print(hex(i))
+print(oct(i))
+
+print(math.pi)
+print(math.hypot(7,24))
+
+x = decimal.Decimal("123456789.9876543210123456789")
+print(x)
+
+print("QQ\nTT")
+
+s = "This is a book"
+print (s[:8] + 'not' + s[9:])
+
+print(s.lower())
+print(s.upper())
+
+
+
+def print_x_times(msg,counts):
+    i = 0
+    while True:
+        if i < counts:
+            print(msg)
+            i += 1    #沒有 i++
+        else:
+            break
+
+print_x_times("XD",3)
+
+
+
