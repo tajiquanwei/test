4779d977a05f89e5da316b5732f607fe2fc4debb
tajiquanwei <tajiquanwei@gmail.com>
2018-03-01 08:12:54 +0800

    123

--- test02.py
+++ test02.py
@@ -1,3 +1,41 @@
-from numpy import *
+import numpy as np
+np1 = np.array([1, 2, 3])
+np2 = np.array([3, 4, 5])
 
-myMat = mat(random.rand(4,4))
+# 陣列相加
+print(np1 + np2) # [4 6 8]
+
+
+# import turtle
+#
+# screen = turtle.Screen()
+#
+# screen.setup(500, 500)
+# myTurtle = turtle.Turtle()
+#
+# x=1
+# y = 1
+# myTurtle.circle(100)
+#
+# # while True:
+# #     if x > 20:
+# #         break
+# #     else:
+# #         myTurtle.forward(x)
+# #         myTurtle.left(90)
+# #         x = x + y
+#
+#
+# screen.exitonclick()
+
+
+# from tkinter import *
+#
+# window = Tk()
+# window.winfo_height()
+# window.winfo_width()
+# label = Label(window, text = "test")
+# label.pack()
+# window.mainloop()
+
+
